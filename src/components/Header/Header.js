ff208f1e42528c03625ba9f78116d0e75ea37e48
Aun <aun@oneminutebookreview.com>
2022-01-31 21:02:07 +0000

    Changes to header and hero sections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,51 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
+  <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
+          <Span>Aun Abdi</Span>
         </a>
       </Link>
     </Div1>
-  </div>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/aunabdi1993">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/aun-abdi-50785780/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.youtube.com/c/AunAbdi/videos">
+        <AiFillYoutube size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.instagram.com/aun.abdi/?hl=en">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,21 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello my name is <br />
+        Aun Abdi. <br />
+        Welcome to my Portfolio Website. 
+      </SectionTitle>
+      <SectionText>
+        I'm a Full-Stack Developer aspiring to create the most beautiful and well-structured web applications possible. 
+      </SectionText>
+      <Button onClick={() => window.location = ''}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
