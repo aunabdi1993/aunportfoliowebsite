5765d8f7e57940aa8f9d3e3fa9ded2ea7ee23c5b
Aun <aun@oneminutebookreview.com>
2022-03-28 12:49:17 +0100

    added contact component to website and deleted two unused projects

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+
+const contact = () => {
+  return (
+<Section id="contact">
+      <SectionTitle>Contact</SectionTitle>
+      <SectionText>
+        Fill out the form below to contact me.
+      </SectionText>
+    </Section>
+  )
+}
+
+export default contact
