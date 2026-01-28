a5f4727d5099c82cd0a1a29907205bd934697fca
Aun Abdi <aabdidevelopment@gmail.com>
2026-01-28 01:41:18 +0000

    added new project

---
+++ .next/server/vendor-chunks/markdown-extensions.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/markdown-extensions";
+exports.ids = ["vendor-chunks/markdown-extensions"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/markdown-extensions/index.js":
+/*!***************************************************!*\
+  !*** ./node_modules/markdown-extensions/index.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// TODO: Load the JSON file when Node.js supports that.\n\nconst markdownExtension = [\n\t'md',\n\t'markdown',\n\t'mdown',\n\t'mkdn',\n\t'mkd',\n\t'mdwn',\n\t'mkdown',\n\t'ron',\n];\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (markdownExtension);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvbWFya2Rvd24tZXh0ZW5zaW9ucy9pbmRleC5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUE7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7O0FBRUEsaUVBQWUsaUJBQWlCLEVBQUMiLCJzb3VyY2VzIjpbIi9Vc2Vycy9hdW5hYmRpL0RvY3VtZW50cy9HaXRIdWIvYXVucG9ydGZvbGlvd2Vic2l0ZS9ub2RlX21vZHVsZXMvbWFya2Rvd24tZXh0ZW5zaW9ucy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBUT0RPOiBMb2FkIHRoZSBKU09OIGZpbGUgd2hlbiBOb2RlLmpzIHN1cHBvcnRzIHRoYXQuXG5cbmNvbnN0IG1hcmtkb3duRXh0ZW5zaW9uID0gW1xuXHQnbWQnLFxuXHQnbWFya2Rvd24nLFxuXHQnbWRvd24nLFxuXHQnbWtkbicsXG5cdCdta2QnLFxuXHQnbWR3bicsXG5cdCdta2Rvd24nLFxuXHQncm9uJyxcbl07XG5cbmV4cG9ydCBkZWZhdWx0IG1hcmtkb3duRXh0ZW5zaW9uO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/markdown-extensions/index.js\n");
+
+/***/ })
+
+};
+;
